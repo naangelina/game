9fbe6eab146bae5063049bc79d2a8f12681d1a29
Angelina <thatsgrrreat@gmail.com>
2023-03-03 12:53:15 -1000

    fixed bug

--- subwaytron.js
+++ subwaytron.js
@@ -1,15 +1,10 @@
-
-
 function use() {
-    //define jumper start position and dimensions
     this.x = w/2
-    this.y = 70
+    this.y = 60
     this.width = 30
     this.height = 30
 
 
-
-    //show what object looks like
     this.show = function() {
         fill(0,230,20)
         rect(this.x,this.y,this.width,this.height)
@@ -17,16 +12,10 @@ function use() {
 
     //handle updating the object
     this.update = function() {
-        
-
-        if (this.y < 50) {
-            this.y = 50
-            this.velocity = 0
+        if (this.y < 60) {
+            this.y = 60
         }
 
-        if (this.x ) {
-            this.x = 0
-        }
 
         if (this.x > (w-30)) {
             this.x = w - 30
