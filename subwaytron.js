19390af7416b1059ffece2b4b823f730fb9e188f
Angelina <110573647+naangelina@users.noreply.github.com>
2023-04-28 03:22:08 -1000

    Replace images + Stamina + Score

--- subwaytron.js
+++ subwaytron.js
@@ -2,8 +2,8 @@ function use() {
     
     this.x = w/2
     this.y = 57
-    this.width = 30
-    this.height = 30
+    this.width = 36
+    this.height = 40
     this.lift = -10
     this.velocity = 0
     this.hp = 100; 
@@ -11,8 +11,8 @@ function use() {
 
 
     this.show = function() {
-        rect(this.x,this.y,this.width,this.height)
-        fill('green')
+        image(peng,this.x,this.y,this.width,this.height)
+        fill('#D3D3D3')
 
     }
 
@@ -24,12 +24,12 @@ function use() {
             this.y = 0
         }
 
-        if (this.y > h-30) {
-            this.y = h-30
+        if (this.y > h-40) {
+            this.y = h-40
         }
 
-        if (this.x > (w-30)) {
-            this.x = w-30
+        if (this.x > (w-36)) {
+            this.x = w-36
         
         }
 
@@ -47,8 +47,11 @@ function userHP() {
     this.height = 5
 
     this.show = function() {
-        rect(use.x -5, use.y + 35, this.width*(use.hp/use.setHp), this.height)
-        fill('red')
+        rect(use.x -3 , use.y + 45, 40, this.height)
+        fill('green')
+
+        rect(use.x -3, use.y + 45, this.width*(use.hp/use.setHp), this.height)
+        fill('#D3D3D3') //HPbar
 
     }
     this.update = function() {
@@ -68,38 +71,74 @@ function userHP() {
 }
 
 function HPbar() {
-    this.x = 5
-    this.y = 5
-    this.width = 100 * (w/5)
-    this.height = 20
+    this.x = 10
+    this.y = 10
+    this.setWidth = w/2.5
+    this.wHeal = this.setWidth/5
+
+
+    this.width = w/2.5
+    this.height = 15
+
+    
+
 
     this.show = function() {
+        rect(this.x,this.y,w/2.5,this.height)
+        fill('green')
         rect(this.x,this.y,this.width,this.height)
+        fill(0) //bullet
+
     }
 }
 
+function sprint() {
+    this.x = 10
+    this.y = 25
+    this.height = 8
+    this.width = w/2.5
+    this.setWidth = w/2.5
+
+    this.drain = w/750
+
+
+    this.show = function() {
+        fill('white')
+        rect(this.x,this.y,this.width,this.height)
+        fill(0)
+
+    }
+}
+
+
 class zombie {
     constructor() {
-        this.x = random(158,w-20)
-        this.y = random(57,h-20)
+        this.x = random(158,w-40)
+        this.y = random(57,h-40)
         this.speed = random(0.5,2)
         this.hp = 20; 
         this.dmg = 15
+        this.hpdmg = HPbar.width/6.6
     }
 
     draw() {
-        ellipse(this.x,this.y,20,20) 
+
+        image(z1,this.x,this.y,30,49) 
         var zo = createVector (this.x - use.x, this.y - use.y)
         zo.normalize()
 
         fill('pink')
 
         if (!zombHit) {
-            zombHit = collideRectRect(use.x,use.y,use.width,use.height,this.x,this.y,20,20)
+            zombHit = collideRectRect(use.x,use.y,use.width,use.height,this.x,this.y,30,49)
             if(zombHit && !inv)
             {
                 inv = true;
+                HPbar.width -= this.hpdmg
                 use.hp -= this.dmg;
+                hits.setVolume(0.5)
+                hits.play()
+                hits.setLoop(false)
                 setTimeout(function()
                 {
                     inv = false
@@ -118,22 +157,21 @@ class zombie {
         
     }
 
-    takeDamage(dmg)
-    {
+    takeDamage(dmg) {
         this.hp -= dmg; 
     }
 }
 
 class hpPack {
     constructor() {
-        this.x = random(158,w)
-        this.y = random(57,h)
+        this.x = random(158,w-30)
+        this.y = random(57,h-30)
         this.hpHit = false;
     }
 
     draw() {
-        rect(this.x,this.y,20,20) 
-        this.hpHit = collideRectCircle(this.x-10,this.y-10,20,20, use.x, use.y, use.width);
+        image(fish,this.x,this.y,30,30) 
+        this.hpHit = collideRectCircle(this.x-10,this.y-10,30,30, use.x, use.y, use.width);
         
         // if (!hpHit) {
         //     hpHit = collideRectCircle(this.x,this.y,20,20, use.x, use.y, use.width);
